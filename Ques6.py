958efd8be46af36e351d0ce216c92753db5833da
HRRSH471 <hariomharish50gmail.com>
2026-05-04 15:56:21 +0530

    Quest 6 done

--- Ques6.py
+++ Ques6.py
@@ -1,4 +1,11 @@
-cart_items=5
-selected_item=3
-updated_cart_item=cart_items+selected_item
-print(f"After 3 increased Shopping Card Items= {updated_cart_item}")
+# Increase Shopping Cart items
+cart_items = 5
+
+print(f"Current Available Items In Cart = {cart_items}")
+print("Enter item to add in cart:")
+
+add_item = int(input())   # take input and convert to integer
+
+final_cart_item = cart_items + add_item
+
+print(f"Final Available Items In Cart = {final_cart_item}")
