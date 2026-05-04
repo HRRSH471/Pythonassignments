96c75572450510e48e4b2e1461612267418e65a6
HRRSH471 <hariomharish50gmail.com>
2026-05-04 16:19:04 +0530

    Question 7 done

--- Ques7.py
+++ Ques7.py
@@ -1,1 +1,7 @@
-
+# Add 20 % discount to a price
+print("Enter the price of the product:")
+price=int(input()) # This take price from user
+print("Enter the discount %..")
+discount=int(input()) # This take discount from user
+final_price=price-price*discount/100
+print(f"Final Price After Discount={final_price}")
