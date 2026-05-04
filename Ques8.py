d4a7431c52fab5ea7a9e8003fff6e99721df8aab
HRRSH471 <hariomharish50gmail.com>
2026-05-04 16:28:16 +0530

    Question8 Done

---
+++ Ques8.py
@@ -0,0 +1,7 @@
+# Calculate student's percentage of marks
+print("Enter the obtain marks of student")
+obtain_marks=int(input())
+print("Enter the total marks of student")
+out_off=int(input())
+percentage=obtain_marks/out_off*100
+print("Percentage of Student=",percentage)
