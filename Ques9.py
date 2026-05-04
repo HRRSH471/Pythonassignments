569bd80e5d330b508dcc9b9a056a552485a4eca8
HRRSH471 <hariomharish50gmail.com>
2026-05-04 16:57:07 +0530

    question 9 done

---
+++ Ques9.py
@@ -0,0 +1,14 @@
+# Calculate Average of Student Marks
+print("Enter the marks of subject 1=")
+sub1=int(input())
+print("Enter the marks of subject 2=")
+sub2=int(input())
+print("Enter the marks of subject 3=")
+sub3=int(input())
+print("Enter the marks of subject 4=")
+sub4=int(input())
+print("Enter the marks of subject 5=")
+sub5=int(input())
+total_mark=sub1+sub2+sub3+sub4+sub5
+averagemarks=total_mark/5
+print(f"Total Marks={total_mark} and Average Marks {averagemarks}")
